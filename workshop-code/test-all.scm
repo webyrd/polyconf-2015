d43ca1cfb205cc44315f80c19602699b54c3cd6c
William E. Byrd <webyrd@gmail.com>
2015-07-05 20:04:08 +0200

    added code from workshop

---
+++ workshop-code/test-all.scm
@@ -0,0 +1,25 @@
+(load "../faster-miniKanren/test-check.scm")
+
+(printf "==-tests\n")
+(load "==-tests.scm")
+
+(printf "symbolo-tests\n")
+(load "symbolo-tests.scm")
+
+(printf "numbero-tests\n")
+(load "numbero-tests.scm")
+
+(printf "symbolo-numbero-tests\n")
+(load "symbolo-numbero-tests.scm")
+
+(printf "disequality-tests\n")
+(load "disequality-tests.scm")
+
+(printf "absento-closure-tests\n")
+(load "absento-closure-tests.scm")
+
+(printf "absento-tests\n")
+(load "absento-tests.scm")
+
+(printf "test-infer\n")
+(load "test-infer.scm")
